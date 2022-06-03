ce8bc2d2616f4b0e135a0e0813f52000a4ea1d5e
HenryLu <73047597+HouyuanLu@users.noreply.github.com>
2022-06-03 11:19:50 +0800

    Add files via upload

---
+++ cplusLITE.h
@@ -0,0 +1,38 @@
+#ifndef CPLUS_H
+#define CPLUS_H
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <cctype>
+#include <istream>
+#include <ostream>
+#include <vector>
+#include <math.h>
+#include <array>
+#include <random>
+#include <windows.h>
+#include <bitset>
+#include <cfloat>
+#include <climits>
+#include <limits>
+#include <list>
+#include <map>
+#include <set>
+#include <iterator>
+#include <iomanip>
+#include <iosfwd>
+#include <ios>
+#include <bit>
+
+
+
+
+#endif
